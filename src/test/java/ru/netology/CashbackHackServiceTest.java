7ff72adb96f26a12768d8807b8b14bf31207a7af
Valentin Fadeev <valentinfs@yandex.ru>
2021-10-19 00:10:03 +0300

    TestNG added

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+public class CashbackHackServiceTest extends Assert {
+
+    CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+   public void shouldReturn100IfAmountIs900() {
+        int amount = 900;
+        int actual = cashback.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn900IfAmountIs1100() {
+        int amount = 1100;
+        int actual = cashback.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+}
