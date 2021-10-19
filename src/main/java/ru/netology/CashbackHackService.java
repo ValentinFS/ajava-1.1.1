74f96d16c1fb1a170b7c08108701ad83757b27a0
Valentin Fadeev <valentinfs@yandex.ru>
2021-10-19 20:54:39 +0300

    CashbackHackService.java changed

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -4,25 +4,8 @@ public class CashbackHackService {
 
     private int boundary = 1000;
 
-    public int remain(int amount){
+    public int remain(int amount) {
         return boundary - amount % boundary;
     }
-
-
-
-
-
-
-//    public int calculateBonus(int amount) {
-//        if (amount < 1000) {
-//            System.out.println("");
-//            return 0;
-//        }
-//        int bonus = (amount - 1000) / 100;
-//        if (bonus > 100) {
-//            return 100;
-//        }
-//        return bonus;
-//    }
 
 }
